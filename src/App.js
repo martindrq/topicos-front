213a0c98d6a94df135f6800183cc4ef063af27ce
Maicool <maihcool@gmail.com>
2021-10-21 21:24:45 -0300

    Squashed commit of the following:
    commit e45ddf0ac1710bd0e6906c7c11ef69f46747f618
    Author: Maicool <maihcool@gmail.com>
    Date:   Thu Oct 21 21:16:05 2021 -0300
    
        Arreglos Links
    
    commit 75b2f8a0f274872282c1ff16d70cf7cbce836eae
    Author: Maicool <maihcool@gmail.com>
    Date:   Thu Oct 21 21:07:08 2021 -0300
    
        Arreglos
    
    commit f910e3f085850994adb9f4079cba0ceb1e3c873b
    Author: Maicool <maihcool@gmail.com>
    Date:   Thu Oct 21 21:05:48 2021 -0300
    
        Cambios
    
    commit 6fb6195cf9e597f34f830812ec05aa1a89039711
    Author: Maicool <maihcool@gmail.com>
    Date:   Thu Oct 21 20:59:35 2021 -0300
    
        Arreglos Indicador
    
        Arreglos INdicador
    
    commit bca303b39fe578084c37ed275da4593d54d72446
    Author: Maicool <maihcool@gmail.com>
    Date:   Thu Oct 21 19:45:11 2021 -0300
    
        Arreglo Icon Home
    
    commit a988383866f5811da289ff92e2c937647897e56d
    Author: Maicool <maihcool@gmail.com>
    Date:   Thu Oct 21 15:48:24 2021 -0300
    
        Minimo cambio.
    
    commit 32d57d9cc8046d9b9ffa8b5ca4fdd96b7bf48fbc
    Author: Maicool <maihcool@gmail.com>
    Date:   Thu Oct 21 15:45:16 2021 -0300
    
        Cambios.
    
        Entendiendo mejor como usar los state.

--- src/App.js
+++ src/App.js
@@ -6,17 +6,12 @@ import { BrowserRouter, Route, Redirect } from 'react-router-dom';
 
 //to-do add react router, and import components from /screens
 import Home from './screens/Home';
-import NotFound from './screens/NotFound';
-import AddIndicator from './screens/AddIndicator';
+import CreateIndicator from './screens/CreateIndicator';
 
 import Box from '@mui/material/Box';
 
 import Login from "./components/Login";
 import Signup from "./components/Signup";
-
-//  <Route component={NotFound} />
-//  <Route path="/" render={() => <Redirect to="/home"/>} />
-//  <Route path="/home" component={Home} exact/>
 
 function App() {
   return (
@@ -25,11 +20,11 @@ function App() {
         <PageLayout>
           <Box sx={{ flexGrow:1 }}>
             <Grid item xs={12} md={12} lg={12}>
-              <Route path="/" render={() => <Redirect to="/home"/>} />
-              <Route path="/home" component={Home} exact/>
-              <Route path="/add-indicator" component={AddIndicator} exact/>
-              <Route path="/login" component={Login} exact/>
-              <Route path="/signup" component={Signup} exact/>
+              <Route path="/" render={() => <Redirect to="/inicio"/>} />
+              <Route path="/inicio" component={Home} exact/>
+              <Route path="/iniciar-sesion" component={Login} exact/>
+              <Route path="/registrarse" component={Signup} exact/>
+              <Route path="/indicador/crear" component={CreateIndicator} exact/>
             </Grid>
           </Box>
         </PageLayout>
