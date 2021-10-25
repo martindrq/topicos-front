388d542b34ec692c5791d1d439ac325af434025f
Maicool <maihcool@gmail.com>
2021-10-25 13:15:19 -0300

    Actualizado del Home

---
+++ src/data.js
@@ -0,0 +1,99 @@
+export default [
+    {
+        id: 1,
+        name: "Consumo de energia",
+        unit: "kw/H",
+        frecuency: "1",
+        type: "Directa",
+        description: "Toma directa realizada por la empresa de un modulo de energia",
+        date: "08/10/2021"
+      },{
+        id: 1,
+        name: "Consumo de energia",
+        unit: "kw/H",
+        frecuency: "1",
+        type: "Directa",
+        description: "Toma directa realizada por la empresa de un modulo de energia",
+        date: "08/10/2021"
+      },{
+        id: 1,
+        name: "Consumo de energia",
+        unit: "kw/H",
+        frecuency: "1",
+        type: "Directa",
+        description: "Toma directa realizada por la empresa de un modulo de energia",
+        date: "08/10/2021"
+      },{
+        id: 1,
+        name: "Consumo de energia",
+        unit: "kw/H",
+        frecuency: "1",
+        type: "Directa",
+        description: "Toma directa realizada por la empresa de un modulo de energia",
+        date: "08/10/2021"
+      },{
+        id: 1,
+        name: "Consumo de energia",
+        unit: "kw/H",
+        frecuency: "1",
+        type: "Directa",
+        description: "Toma directa realizada por la empresa de un modulo de energia",
+        date: "08/10/2021"
+      },{
+        id: 1,
+        name: "Consumo de energia",
+        unit: "kw/H",
+        frecuency: "1",
+        type: "Directa",
+        description: "Toma directa realizada por la empresa de un modulo de energia",
+        date: "08/10/2021"
+      },{
+        id: 1,
+        name: "Consumo de energia",
+        unit: "kw/H",
+        frecuency: "1",
+        type: "Directa",
+        description: "Toma directa realizada por la empresa de un modulo de energia",
+        date: "08/10/2021"
+      },{
+        id: 1,
+        name: "Consumo de energia",
+        unit: "kw/H",
+        frecuency: "1",
+        type: "Directa",
+        description: "Toma directa realizada por la empresa de un modulo de energia",
+        date: "08/10/2021"
+      },{
+        id: 1,
+        name: "Consumo de energia",
+        unit: "kw/H",
+        frecuency: "1",
+        type: "Directa",
+        description: "Toma directa realizada por la empresa de un modulo de energia",
+        date: "08/10/2021"
+      },{
+        id: 1,
+        name: "Consumo de energia",
+        unit: "kw/H",
+        frecuency: "1",
+        type: "Directa",
+        description: "Toma directa realizada por la empresa de un modulo de energia",
+        date: "08/10/2021"
+      },{
+        id: 1,
+        name: "Consumo de energia",
+        unit: "kw/H",
+        frecuency: "1",
+        type: "Directa",
+        description: "Toma directa realizada por la empresa de un modulo de energia",
+        date: "08/10/2021"
+      },{
+        id: 1,
+        name: "Consumo de energia",
+        unit: "kw/H",
+        frecuency: "1",
+        type: "Directa",
+        description: "Toma directa realizada por la empresa de un modulo de energia",
+        date: "08/10/2021"
+      }
+]
