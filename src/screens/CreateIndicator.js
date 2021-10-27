95477d396b30865f723e0f1fec69c0e999a6a1e5
Maicool <maihcool@gmail.com>
2021-10-27 14:45:46 -0300

    Plantilla toma y envió de datos

--- src/screens/CreateIndicator.js
+++ src/screens/CreateIndicator.js
@@ -1,22 +1,39 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import {TextField, Button, Radio, RadioGroup, FormControlLabel, FormControl, FormLabel, Stack, InputLabel, MenuItem, FormHelperText, Select} from '@mui/material/';
-import AddIcon from '@mui/icons-material/Add';
+import {Add} from '@mui/icons-material';
+
+import listUnits from '../listUnits.js'
+import listIndicators from '../listIndicators.js'
+import listOperators from '../listOperators.js'
+import listFrequency from '../listFrequency'
+
 
 const AddIndicator = () => { 
     
-    const [unidad, setUnidad] = useState('');
-    const [indicadorValue1, setIndicadorValue1] = useState('');
-    const [indicadorValue2, setIndicadorValue2] = useState('');
-    const [operadorValue1, setOperadorValue1] = useState('');
-    const [frecuencia, setFrecuencia] = useState('');
-
-    const [radioSelect, setRadioSelect] = useState('D')
     const [disable, setDisable] = useState(true);
 
-    const handleChangeRadioSelect = (event,) => {
-        setRadioSelect(event.target.value)
-        hide(event.target.value)
+    const [datosForm, setDatosForm] = useState({
+        name: '',
+        description: '',
+        unidad: '',
+        indicadorValue1: '',
+        indicadorValue2: '',
+        operadorValue1: '',
+        frecuencia: '',
+        radioSelect: 'D'
+    })
+    
+    const [error, setError] = useState(false)
+    
+    const handleInputChange = (event) => {
+        setDatosForm({
+            ...datosForm,
+            [event.target.name] : event.target.value
+        })
+
+        if (event.target.name === 'radioSelect')
+            hide(event.target.value)
     }
 
     function hide(state){
@@ -52,61 +69,60 @@ const AddIndicator = () => {
             setError(true);
         }
         else {
-            setHelperText('You got it!');
-            setError(false);
+            setError(false)
         }
-    };
-  
-    
+        console.log('Enviando datos...  ' + datosForm.unidad + ' ' + datosForm.indicadorValue1 + ' ' + datosForm.indicadorValue2 + ' '  + datosForm.operadorValue1 + ' '  + datosForm.frecuencia + ' ' +  datosForm.description)
+    }
+
     return(
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={enviarDatosForm}>
             <Stack direction="column" alignItems="center" spacing={2} >
                 <FormLabel component="legend">Indicador</FormLabel>
-                <TextField id="id_name" label="Nombre" variant="outlined" fullWidth autocomplete="none" error={error}/>
-                <FormHelperText>{helperText}</FormHelperText>
-
-                <TextField id="id_description" label="Descripción" variant="outlined" fullWidth multiline rows={5} autocomplete="none" error={error}/>
-                <FormHelperText>{helperText}</FormHelperText>
-                
+                <TextField label="Nombre" variant="outlined" fullWidth required autocomplete="none" 
+                    name='name'
+                    value={datosForm.name}
+                    onChange={handleInputChange}
+                />
+                <TextField label="Descripción" variant="outlined" fullWidth multiline rows={5} autocomplete="none" 
+                    name='description'
+                    value={datosForm.description}
+                    onChange={handleInputChange} 
+                />
                 <Stack direction="row" alignItems="center" spacing={0}>
-                    <FormControl sx={{ m: 1, minWidth: 2 }} error={error}>
-                        <InputLabel id="u">Unidad</InputLabel>
-                        <Select
-                            labelId="u"
-                            id="id_unidad"
-                            value={unidad}
-                            label=""
-                            onChange={(e) => setUnidad(e.target.value)}
+                    <FormControl sx={{ m: 1, minWidth: 2 }}>
+                        <InputLabel id="u">Unidad *</InputLabel>
+                        <Select labelId="u" 
+                            name='unidad'
+                            value={datosForm.unidad}
+                            onChange={handleInputChange}
+                            error={error}
                         >
                             <MenuItem value=""><em>None</em></MenuItem>
-                            <MenuItem value={10}>L</MenuItem>
-                            <MenuItem value={20}>m3</MenuItem>
-                            <MenuItem value={30}>US$</MenuItem>
+                            {listUnits.map( (unit) => <MenuItem value={unit.id}>{unit.name}</MenuItem> )}
                         </Select>
                         <FormHelperText>Métrica de medida</FormHelperText>
                     </FormControl>
 
                     <FormControl sx={{ m: 1, minWidth: 2 }}>
                         <InputLabel id="f">Frecuencia</InputLabel>
-                        <Select
-                            labelId="f"
-                            id="id_frecuencia"
-                            value={frecuencia}
-                            label=""
-                            onChange={(e) => setFrecuencia(e.target.value)}
-                        >
-                            <MenuItem value=""><em>None</em></MenuItem>
-                            <MenuItem value={1}>1</MenuItem>
-                            <MenuItem value={2}>2</MenuItem>
-                            <MenuItem value={3}>3</MenuItem>
-                        </Select>
+                            <Select
+                                labelId="f"
+                                id="id_frecuencia"
+                                value={datosForm.frecuencia}
+                                label=""
+                                name='frecuencia'
+                                onChange={handleInputChange}
+                            >
+                                <MenuItem value=""><em>None</em></MenuItem>
+                                {listFrequency.map( (frequency) => <MenuItem value={frequency.id}>{frequency.name}</MenuItem> )}
+                            </Select>
                         <FormHelperText>Frecuencia de medida en meses</FormHelperText>
                     </FormControl>
 
                 </Stack> 
                 <FormControl>
                     <FormLabel>Formula</FormLabel>
-                        <RadioGroup row value={radioSelect} onChange={handleChangeRadioSelect}>
+                        <RadioGroup row name='radioSelect' value={datosForm.radioSelect} onChange={handleInputChange}>
                             <FormControlLabel value='D' control={<Radio color="success" />} label="Directa" />
                             <FormControlLabel value='I' control={<Radio color="secondary" />} label="Indirecta" />
                         </RadioGroup>
@@ -118,14 +134,13 @@ const AddIndicator = () => {
                         <Select
                             labelId="i1"
                             id="id_indicador1"
-                            value={indicadorValue1}
+                            value={datosForm.indicadorValue1}
                             label=""
-                            onChange={(e) => setIndicadorValue1(e.target.value)}
+                            name='indicadorValue1'
+                            onChange={handleInputChange}
                         >
                             <MenuItem value=""><em>None</em></MenuItem>
-                            <MenuItem value={10}>Indicador 1</MenuItem>
-                            <MenuItem value={20}>Indicador 2</MenuItem>
-                            <MenuItem value={30}>Indicador 3</MenuItem>
+                            {listIndicators.map( (indicator) => <MenuItem value={indicator.id}>{indicator.name}</MenuItem> )}
                         </Select>
                         <FormHelperText>Seleccione el primer indicador</FormHelperText>
                     </FormControl>
@@ -134,15 +149,13 @@ const AddIndicator = () => {
                         <InputLabel id="i1">Operador</InputLabel>
                         <Select
                             id="id_operador_1"
-                            value={operadorValue1}
+                            value={datosForm.operadorValue1}
                             label=""
-                            onChange={(e) => setOperadorValue1(e.target.value)}
+                            name='operadorValue1'
+                            onChange={handleInputChange}
                         >
                             <MenuItem value=""><em>None</em></MenuItem>
-                            <MenuItem value={1}>/</MenuItem>
-                            <MenuItem value={2}>*</MenuItem>
-                            <MenuItem value={3}>+</MenuItem>
-                            <MenuItem value={4}>-</MenuItem>
+                            {listOperators.map( (operator) => <MenuItem value={operator.id}>{operator.name}</MenuItem> )}
                         </Select>
                         <FormHelperText>Seleccione el operador</FormHelperText>
                     </FormControl>
@@ -152,14 +165,13 @@ const AddIndicator = () => {
                         <Select
                             labelId="i2"
                             id="id_indicador2"
-                            value={indicadorValue2}
+                            value={datosForm.indicadorValue2}
                             label=""
-                            onChange={(e) => setIndicadorValue2(e.target.value)}
+                            name='indicadorValue2'
+                            onChange={handleInputChange}
                         >
                             <MenuItem value=""><em>None</em></MenuItem>
-                            <MenuItem value={10}>Indicador 1</MenuItem>
-                            <MenuItem value={20}>Indicador 2</MenuItem>
-                            <MenuItem value={30}>Indicador 3</MenuItem>
+                            {listIndicators.map( (indicator) => <MenuItem value={indicator.id}>{indicator.name}</MenuItem> )}
                         </Select>
                         <FormHelperText>Seleccione el segundo indicador</FormHelperText>
                     </FormControl>
@@ -172,7 +184,7 @@ const AddIndicator = () => {
                         type="submit" 
                         color="primary" 
                         variant="contained"
-                        endIcon={<AddIcon />}>
+                        endIcon={<Add/>}>
                         Crear
                     </Button>
                 </Stack>  
