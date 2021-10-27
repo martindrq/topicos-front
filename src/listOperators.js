238eeb861741af6a9efed4c2ed6544f5c0aa9070
Maicool <maihcool@gmail.com>
2021-10-27 14:44:42 -0300

    Add Js Recibido Prueba

---
+++ src/listOperators.js
@@ -0,0 +1,15 @@
+export default [
+    {
+        id: 1,
+        name: "+",
+        },{
+        id: 2,
+        name: "-",
+    },{
+        id: 3,
+        name: "*",
+    },{
+        id: 4,
+        name: "/",
+    }
+]
