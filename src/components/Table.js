642902e2c66f0e9803f91b29320030dd97cec39a
Maicool <maihcool@gmail.com>
2021-11-11 16:03:00 -0300

    Agrego boton, filtra bien, muestra mal

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,17 +1,18 @@
-import * as React from 'react';
-import Paper from '@mui/material/Paper';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TablePagination from '@mui/material/TablePagination';
-import TableRow from '@mui/material/TableRow';
+import React, { useState } from 'react';
+import {Paper, Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow, Checkbox, Toolbar, Typography, FormControl, InputLabel, Select, MenuItem, TextField} from '@mui/material';
+import { SearchOutlined } from '@mui/icons-material';
 
 export default function StickyHeadTable({ columns, rows }) {
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(10);
-
+ 
+  const [listFilter, setListFilter] = useState(rows);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [selected, setSelected] = useState([]);
+  const [action, setAction] = useState('');
+  const [wordSearch, setWordSearch] = useState('');
+ 
+  const isSelected = (id) => selected.indexOf(id) !== -1;
+  
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
   };
@@ -21,8 +22,95 @@ export default function StickyHeadTable({ columns, rows }) {
     setPage(0);
   };
 
+  const handleSelectAllClick = (event) => {
+    if (event.target.checked) {
+      const newSelecteds = listFilter.map((n) => n.id);
+      setSelected(newSelecteds);
+      return;
+    }
+    setSelected([]);
+  };
+
+  const handleClick = (event, id) => {
+  
+    const selectedIndex = selected.indexOf(id);
+    let newSelected = [];
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, id);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+        newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1),
+      );}
+    setSelected(newSelected);
+  };
+
+  const handleChangeAction = (event) => {
+    setAction(event.target.value);
+  };
+
+  const handleWordSearch = (event) => {
+    if (event.target.value === ""){
+      setWordSearch('');
+      setListFilter(rows) 
+    }
+    else {
+      setWordSearch(event.target.value);
+      handleSearch(event.target.value)
+    }
+  };
+
+  const handleSearch = (word) => {
+    const newList = rows.filter(
+      element => {
+        return ( element.name.toLowerCase().includes(word.toLowerCase()));
+      })
+    setListFilter(newList)
+  };
+
   return (
     <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+      <Toolbar>
+        <TextField
+          type = "search" 
+          variant="standard"
+          value= {wordSearch}
+          onChange= {handleWordSearch}
+          placeholder=" Buscar..."
+          InputProps={{
+            startAdornment: <SearchOutlined fontSize="small" />,
+          }}
+        />
+        {selected.length > 0 ? (
+        <TableContainer sx={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'flex-end',
+        }}
+        > 
+          <Typography>
+            {selected.length} seleccionados
+          </Typography>
+
+          <FormControl variant="filled" sx={{ minWidth: 120}}>
+            <InputLabel id="a">Acción</InputLabel>
+              <Select
+                labelId ='a'
+                value={action}
+                onChange={handleChangeAction}
+              >
+                <MenuItem value="">
+                  <em>-</em>
+                </MenuItem>
+                <MenuItem value={1}>Borrar</MenuItem>
+               </Select>
+          </FormControl>
+        </TableContainer> ) : null}
+    </Toolbar>
       <TableContainer sx={{ maxHeight: 440 }}>
         <Table stickyHeader aria-label="sticky table">
           <TableHead>
@@ -39,7 +127,7 @@ export default function StickyHeadTable({ columns, rows }) {
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows
+            {listFilter
               .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
               .map((row) => {
                 return (
