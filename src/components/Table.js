d0b8531f6bd9a8cbf6e566db92a457ba0ba99970
agus asap <afernandez@asapdevelopers.com>
2021-11-11 19:57:20 -0300

    clean code

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,7 +1,5 @@
 import React, { useState } from 'react';
-import {Paper, Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow, Checkbox, Tooltip, IconButton, Toolbar, Typography, FormControl, InputLabel, Select, MenuItem} from '@mui/material';
-import { Delete } from '@mui/icons-material';
-
+import {Paper, Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow, Checkbox, Toolbar, Typography, FormControl, InputLabel, Select, MenuItem} from '@mui/material';
 
 export default function StickyHeadTable({ columns, rows }) {
   const [page, setPage] = useState(0);
